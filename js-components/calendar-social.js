a65a33c71e9098e83d9d3add1013c84348f61ac3
Mat Bishop <spam@emgeebee.com>
2015-06-13 11:21:40 +0100

    new angular app

--- js-components/calendar-social.js
+++
@@ -1,9 +0,0 @@
-var loadLibrary = function(d,s,id,url){
-	var js,fjs=d.getElementsByTagName(s)[0];
-	if(!d.getElementById(id)){
-		js=d.createElement(s);
-		js.id=id;
-		js.src=url;
-		fjs.parentNode.insertBefore(js,fjs);
-	}
-}
