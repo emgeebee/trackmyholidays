fa0a7d1550aa1e0f19838332cf3ba556e13b8882
Mat Bishop <matthew.g.bishop@accenture.com>
2020-08-02 20:03:50 +0100

    add build

---
+++ build/precache-manifest.86b7ba61d4ee28aa2983c75ba794e052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5622df12e624209027bfb124682ba58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc6fa4285933513c62f8",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "2164f8d23c90e33fdc01",
+    "url": "/static/css/main.010c763b.chunk.css"
+  },
+  {
+    "revision": "bc6fa4285933513c62f8",
+    "url": "/static/js/2.7fa60949.chunk.js"
+  },
+  {
+    "revision": "2164f8d23c90e33fdc01",
+    "url": "/static/js/main.cb3f1497.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
