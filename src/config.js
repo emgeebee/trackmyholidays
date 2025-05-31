b1a52a86903f622c48727960803413e843c09eae
Mat Bishop <mat@emgeebee.com>
2025-05-31 21:14:21 +0100

    config

--- src/config.js
+++ src/config.js
@@ -2,12 +2,49 @@ export const bankHolidayCountries = {
     uk: {
         display: 'UK',
         dates: [
-            {
-                date: ''
-            }
+            { start: '24-01-01', name: 'New Year\'s Day' },
+            { start: '24-03-29', name: 'Good Friday' },
+            { start: '24-04-01', name: 'Easter Monday' },
+            { start: '24-05-06', name: 'Early May Bank Holiday' },
+            { start: '24-05-27', name: 'Spring Bank Holiday' },
+            { start: '24-08-26', name: 'Summer Bank Holiday' },
+            { start: '24-12-25', name: 'Christmas Day' },
+            { start: '24-12-26', name: 'Boxing Day' },
+            { start: '25-01-01', name: 'New Year\'s Day' },
+            { start: '25-04-18', name: 'Good Friday' },
+            { start: '25-04-21', name: 'Easter Monday' },
+            { start: '25-05-05', name: 'Early May Bank Holiday' },
+            { start: '25-05-26', name: 'Spring Bank Holiday' },
+            { start: '25-08-25', name: 'Summer Bank Holiday' },
+            { start: '25-12-25', name: 'Christmas Day' },
+            { start: '25-12-26', name: 'Boxing Day' }
         ]
     },
     usa: {
         display: 'USA',
+        dates: [
+            { start: '24-01-01', name: 'New Year\'s Day' },
+            { start: '24-01-15', name: 'Martin Luther King Jr. Day' },
+            { start: '24-02-19', name: 'Presidents\' Day' },
+            { start: '24-05-27', name: 'Memorial Day' },
+            { start: '24-06-19', name: 'Juneteenth' },
+            { start: '24-07-04', name: 'Independence Day' },
+            { start: '24-09-02', name: 'Labor Day' },
+            { start: '24-10-14', name: 'Columbus Day' },
+            { start: '24-11-11', name: 'Veterans Day' },
+            { start: '24-11-28', name: 'Thanksgiving Day' },
+            { start: '24-12-25', name: 'Christmas Day' },
+            { start: '25-01-01', name: 'New Year\'s Day' },
+            { start: '25-01-20', name: 'Martin Luther King Jr. Day' },
+            { start: '25-02-17', name: 'Presidents\' Day' },
+            { start: '25-05-26', name: 'Memorial Day' },
+            { start: '25-06-19', name: 'Juneteenth' },
+            { start: '25-07-04', name: 'Independence Day' },
+            { start: '25-09-01', name: 'Labor Day' },
+            { start: '25-10-13', name: 'Columbus Day' },
+            { start: '25-11-11', name: 'Veterans Day' },
+            { start: '25-11-27', name: 'Thanksgiving Day' },
+            { start: '25-12-25', name: 'Christmas Day' }
+        ]
     }
 }
